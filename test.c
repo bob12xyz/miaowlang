fc1257fbcf3c3e26bd8748c702b64d97e8d7b3ea
icwtyjj <johnnyzh77@gmail.com>
2025-12-24 13:51:41 -0800

    deleted unused files

--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char hello[] = "hello goodbye";
-    hello[6] = 'b';
-    printf("%s", hello);
-}
--- texturetest.ll
+++
@@ -1,7 +0,0 @@
-; ModuleID = 'miaow_module'
-source_filename = "miaow_module"
-
-define i32 @main() {
-entry:
-  ret i32 0
-}
